a7c51fcb756f1eb59a4cf1f468493b63b55bbd85
Kirill Yukhin <kyukhin@tarantool.org>
2024-12-07 07:08:19 +0200

    Fifth day solutions
    Both.

---
+++ 05/a/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInput(fn string) (rules [][]int, seqs [][]int) {
+	fd, err := os.Open(fn)
+	if err != nil {
+		panic(fmt.Sprintf("open %s: %v", fn, err))
+	}
+	defer fd.Close()
+
+	n1 := 0
+	n2 := 0
+	scanner := bufio.NewScanner(fd)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		_, err := fmt.Sscanf(line, "%d|%d\n", &n1, &n2)
+		if err != nil {
+			panic(fmt.Sprintf("Scan failed %s: %v", fn, err))
+		}
+
+		rules = append(rules, []int{n1, n2})
+	}
+	fmt.Println("Number of rules read:", len(rules))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		slice := strings.Split(line, ",")
+		arr := []int{}
+		for _, e := range slice {
+			x, err := strconv.Atoi(e)
+			if err != nil {
+				panic(fmt.Sprintf("Pages detection %s: %e", line, e))
+			}
+			arr = append(arr, x)
+		}
+		seqs = append(seqs, arr)
+	}
+
+	fmt.Println("Number of sequences read:", len(seqs))
+	return rules, seqs
+}
+
+func check(y, x int, rules [][]int) bool {
+	for _, r := range rules {
+		if r[0] == x && r[1] == y {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	rules, seqs := readInput("input.txt")
+	result := 0
+	for _, s := range seqs {
+		processed := []int{}
+		for _, x := range s {
+			ok := true
+			for _, y := range processed {
+				if !check(y, x, rules) {
+					ok = false
+					break
+				}
+			}
+			if !ok {
+				break
+			}
+			processed = append(processed, x)
+		}
+		if len(processed) == len(s) {
+			if len(s)%2 == 0 {
+				panic("Sequence of even length. Not implemented")
+			}
+			result += s[len(s)/2]
+		}
+	}
+
+	fmt.Println("Result: ", result)
+}
